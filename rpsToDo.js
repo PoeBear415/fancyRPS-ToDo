42d652b147228f801cd0ec79e8d071ae1ca1296e
fruit13ok <fruit13ok@gmail.com>
2017-03-21 18:03:43 -0700

    other stuff

--- rpsToDo.js
+++ rpsToDo.js
@@ -17,42 +17,47 @@
 
 
 var losses = 0;
+var wins = 0;
 //TO DO: enter code to match 'losses' above for 'wins'
   
 //YOUR CODE GOES HERE
 //declare and set 'wins' variable to 0 below
 
 			
-var play = function(playerChoice) {
+var play = function (playerChoice) {
 
   //Start off by defining player, opponent, and results values as empty strings
   //None of these have a value yet, so they are defined as variables for the computer, but "points" are left blank until game play begins
   document.getElementById('player').innerHTML = '';
-  //TO DO: enter code to match 'player' above for 'opponent' and 'results'
-	
+    
+    document.getElementById('results').innerHTML = '';
+    
+    document.getElementById('opponent').innerHTML = '';
+ //TO DO: enter code to match 'player' above for 'opponent' and 'results'
+ 	
   //YOUR CODE GOES HERE 
-
+ 
 	//The user makes their choice, which sets that value (rock, paper, or scissors) to 'player'  
-	//This is our very first Javascript conditional statement!
+ 	//This is our very first Javascript conditional statement!
 	//A conditional statement is where a piece of code gets executed IF something is true. === means a value exactly matches the variable; || stands for "or"
 	//The following conditional says:
 	//"If the player chooses rock, OR chooses paper, OR chooses scissors, then show the player's choice on the page'
-  if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors') {
+   if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors') {
     //When we get that element by the 'player' ID, the user's choice is then printed on the page. HTML + JS's 'document.getElementById' is how we get the value to display.
     document.getElementById('player').innerHTML = 'You chose' + ' ' + playerChoice + '.';
 
   } else {
     //If our page has a random option to choose, like "fire" we can throw an error
-    document.getElementById('player').innerHTML = 'That is not a valid choice, try again.';
-    return false;
-  }
+     document.getElementById('player').innerHTML = 'That is not a valid choice, try again.';
+     return false;
+   }
 	
-	//The computer's selection is set to a random number.  Math.random(); is calling a native Javascript function to provide the random number.  The rest of this function sets the value of the computer choice based on that number. They are broken up into "thirds" because we have three choices. We want to stop at 1/100th above the fraction, so that the computer includes .33 & .66 in the comparison
+	//The computer's selection is set to a random number.  Math.random(); is calling a native Javascript function to   provide the random number.  The rest of this function sets the value of the computer choice based on that number. They are broken up into "thirds" because we have three choices. We want to stop at 1/100th above the fraction, so that the computer includes .33 & .66 in the comparison
   var computerChoice = Math.random();
   //if .33 or less, choice will be rock
   if (computerChoice < 0.34) {
-    computerChoice = 'rock';
-  //
+     
+  computerChoice = 'rock';
   //**HINT: the way to combined two conditional arguments in javascript is with &&:
   // if (peanutButter && jelly){
   //	 make sandwich;
@@ -64,16 +69,19 @@ var play = function(playerChoice) {
   } else if (computerChoice <= 0.67 && computerChoice > 0.33) {
     //TO DO: set computer choice equal to paper here
     //YOUR CODE GOES HERE
-
+ computerChoice = 'paper';
+      
+      
     
   //otherwise, choice will be scissors
   } else {
     //TO DO: set computer choice equal to scissors here
     //YOUR CODE GOES HERE
+                computerChoice = 'scissors';
   } 
 
   //when we get the element by the 'opponent' ID, the computer's choice is then printed on the page
-  document.getElementById('opponent').innerHTML = '// Your opponent chose' + ' ' + computerChoice + '.';
+  document.getElementById('opponent').innerHTML = '// Your opponent chose' + '.' + computerChoice + '.';
 
   //here, we make the game logic to compare the rock, paper, & scissors
   var compare = function (choice1, choice2) {
@@ -116,10 +124,14 @@ var play = function(playerChoice) {
   
 
   document.getElementById('results').innerHTML = winner;
+    
   //TO DO: enter code to match 'winner' above for 'wins' and 'losses'
 
   //YOUR CODE GOES HERE
-					
+	var losses = compare(playerChoice, computerChoice);
+    
+    document.getElementById('results').innerHTML = winner;
+    
   if (wins > 99 || losses > 99) {
     alert('You have played 99 rounds! <br />Take a break and go outside.');
   }
